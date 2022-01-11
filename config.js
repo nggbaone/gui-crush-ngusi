6d94f5bebd4a00f98abd1552b1f16afa53dbdffd
nggbaone <97518349+nggbaone@users.noreply.github.com>
2022-01-11 22:16:21 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 À Nhon Xê Nhô ! 🖐🖐🖐',
+    introDesc: `Tớ có điều này muốn nói với cậu từ lâu rồi`,
+    btnIntro: 'Sao saoo',
+    title: 'Cậu thích tớ lắm đúng không ? 😙',
+    desc: 'hong trả lời hoặc thoát ra, tức là có nhaa :> ',
+    btnYes: 'Có, tớ thích Giabaoo nhiều lắm 💖💖💖',
+    btnNo: '💔 Mơ điiii ! 💔 ',
+    question:'Nói cho tớ biết đi vì sao cậu thích tớ dữ dằn zayy 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì GiaBaoo deeptry, học giỏi neee 😊😊',
+    mess: 'Tớ biết mà, tớ cũng thích cậu lắm 🥰. ai lớp diuuu 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tớ nha. 💬💬',
+    btnAccept: 'hong bé ơiii 💖',
+    messLink: 'https://m.me/100036870168035'
+}
